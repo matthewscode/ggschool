ea0790d6cbfbdf94202972bbbbc6e7ef9228588b
matthewscode <mattennevor@gmail.com>
2017-04-24 19:31:17 -0700

    Initial commit

---
+++ src/main/java/com/ggschool/GgschoolApplication.java
@@ -0,0 +1,13 @@
+package com.ggschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgschoolApplication.class, args);
+		System.out.println("HELLO");
+	}
+}
